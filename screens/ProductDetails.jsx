fb98795c9b3dbe29b00b37adda06d934718a9833
Teta_Lena <tetalenaa@gmail.com>
2023-09-06 01:19:30 +0200

    product details done

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View, FlatList, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { COLORS, SIZES } from "../constants";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  Ionicons,
+  SimpleLineIcons,
+  MaterialCommunityIcons,
+  Fontisto,
+} from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 const ProductDetails = ({ navigation }) => {
   const img = require("../assets/images/darya.jpg");
+  const [count, setCount] = useState(1);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
@@ -24,7 +38,68 @@ const ProductDetails = ({ navigation }) => {
       <View style={styles.details}>
         <View style={styles.titleRow}>
           <Text style={styles.title}>Here</Text>
-          <View style={styles.priceWrapper}></View>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$66</Text>
+          </View>
+        </View>
+
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 5].map((index) => (
+              <Ionicons key={index} name="star" size={24} color="gold" />
+            ))}
+            <Text style={styles.ratingText}>(4.9)</Text>
+          </View>
+          <View style={styles.rating}>
+            <TouchableOpacity
+              onPress={() => {
+                increment();
+              }}
+            >
+              <SimpleLineIcons name="plus" size={20} />
+            </TouchableOpacity>
+            <Text style={styles.ratingText}>{count}</Text>
+
+            <TouchableOpacity
+              onPress={() => {
+                decrement();
+              }}
+            >
+              <SimpleLineIcons name="minus" size={20} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.descriptionWrapper}>
+          <Text style={styles.description}>Description</Text>
+          <Text style={styles.descText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            aspernatur beatae consequuntur! Consequuntur quam nobis adipisci.
+            Dolore, magnam dicta veritatis, laudantium ut quis dolorem iure cum,
+            ipsa doloribus culpa ad.
+          </Text>
+        </View>
+        <View style={{ marginBottom: SIZES.small }}>
+          <View style={styles.location}>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons name="location-outline" size={20} />
+              <Text>Location</Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <MaterialCommunityIcons name="truck-delivery-outline" size={20} />
+              <Text>Free delivery</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.cartRow}>
+          <TouchableOpacity onPress={() => {}} style={styles.cartBtn}>
+            <Text style={styles.cartTitle}>BUY NOW</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}} style={styles.addCart}>
+            {/* <Text style={styles.cartTitle}>BUY NOW</Text> */}
+            <Fontisto name="shopping-bag" size={20} color={COLORS.lightWhite} />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -53,7 +128,7 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
   details: {
-    marginTop: -SIZES.large,
+    marginTop: -SIZES.large - 60,
     backgroundColor: COLORS.lightWhite,
     width: SIZES.width,
     borderTopLeftRadius: SIZES.medium,
@@ -68,9 +143,90 @@ const styles = StyleSheet.create({
     width: SIZES.width - 44,
     top: 20,
   },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width - 10,
+    top: 5,
+  },
+
   title: {
     fontFamily: "bold",
     fontSize: SIZES.large,
   },
-  priceWrapper: {},
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large,
+  },
+  price: {
+    paddingHorizontal: 10,
+    fontFamily: "medium",
+    fontSize: SIZES.large,
+  },
+  rating: {
+    top: SIZES.large,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginHorizontal: SIZES.large,
+  },
+  ratingText: {
+    color: COLORS.gray,
+    fontFamily: "medium",
+    paddingHorizontal: 5,
+  },
+  descriptionWrapper: {
+    marginTop: SIZES.large * 2,
+    marginHorizontal: SIZES.large,
+  },
+  description: {
+    fontFamily: "medium",
+    fontSize: SIZES.large - 2,
+  },
+  descText: {
+    textAlign: "justify",
+    fontFamily: "regular",
+    fontSize: SIZES.small,
+    marginBottom: SIZES.small,
+  },
+  location: {
+    flexDirection: "row",
+    borderRadius: SIZES.medium,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: COLORS.secondary,
+    padding: 5,
+    marginHorizontal: 12,
+  },
+  cartBtn: {
+    width: SIZES.width * 0.7,
+    backgroundColor: COLORS.black,
+    padding: SIZES.small / 2,
+    borderRadius: SIZES.large,
+    marginLeft: 12,
+  },
+  cartTitle: {
+    fontFamily: "medium",
+    fontSize: SIZES.medium,
+    color: COLORS.lightWhite,
+    marginLeft: SIZES.small,
+  },
+  cartRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingBottom: SIZES.small,
+    width: SIZES.width,
+  },
+  addCart: {
+    width: 37,
+    height: 37,
+    borderRadius: 50,
+    margin: SIZES.small,
+    backgroundColor: COLORS.black,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
